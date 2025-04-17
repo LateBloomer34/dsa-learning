b08b4577add054d3fd699a10d67d4ab29f7ae9a4
LateBloomer34 <himnariyal30@gmail.com>
2025-04-17 11:52:56 +0530

    data type in cpp

---
+++ 00Datatype.cpp
@@ -0,0 +1,18 @@
+/*
+in cpp basically 3 types of dataype
+int - 4 byte size
+float - 4 byte
+char - 1 byte
+bool -1 byte
+double  - 8 byte
+void     -  absence of value 
+*/
+
+/*
+int -  integer value, range from 2^-31 to 2^31-1
+float -  floating number means decimal value - 
+char  -  character value enclosed by ' '
+bool - boolen value , either 0 or 1  -  o means false and 1 means true
+double - decimal value with higher precision 
+void - which returns nothing 
+*/
