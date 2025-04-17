99019371dd5c99fd4140ce66162efb8c4cd706db
LateBloomer34 <himnariyal30@gmail.com>
2025-04-17 11:55:03 +0530

    type casting in cpp

---
+++ 00type_Casting.cpp
@@ -0,0 +1,24 @@
+/*
+typecasting means type conversion 
+basically 2 type of typecasting are used in cpp
+1- implicit type casting - automatic type conversion
+2 - explicit type casting  -  type conversion done by user
+
+*/
+
+
+// 1- implicit type casting 
+// examples 
+
+// 1-
+#include<iostream>
+using namespace std;
+
+
+int main (){
+int num1 = 2;
+float num2 = 3.5;
+
+float add = num1+num2;
+cout<<add;
+}
