61ba7eb2fb55bf82e74efc5c0abd43714dd54351
LateBloomer34 <himnariyal30@gmail.com>
2025-05-06 22:29:22 +0530

    sorting

---
+++ sorting/02BubbleSort.cpp
@@ -0,0 +1,24 @@
+// simplest sorting approach, first element is compare with next element, is first is greater than second , swap each other.
+
+#include<iostream>
+using namespace std;
+
+int SS(int arr[] , int n ){
+        for (int i = 0 ; i < n ; i++){
+            for (int j = i ; j < n ; j++){
+                if (arr[i]>arr[j]){
+                    swap(arr[i], arr[j]);
+                }
+            }
+        }
+        for (int i = 0 ; i < n ; i++){
+            cout<<arr[i]<<" ";
+        }
+    return 0;
+}
+
+int main (){
+    int arr[] = {3,5,8,1,2};
+    int n = sizeof arr / sizeof arr[0];
+    SS(arr ,  n );
+}
