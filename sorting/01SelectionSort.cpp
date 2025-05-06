19caad2dcdd7ae9987ae0b8509f172943d73f003
LateBloomer34 <himnariyal30@gmail.com>
2025-05-06 22:47:17 +0530

    selection sort

--- sorting/01SelectionSort.cpp
+++ sorting/01SelectionSort.cpp
@@ -1,17 +1,19 @@
 // selction sort is the basic approach of sorting with TC O(n^2)
-
+// select any element and compare it with all element, if element is lesser then first, swap each other.
 #include<iostream>
 using namespace std;
 
 int SS(int arr[] , int n ){
-        for (int i = 0 ; i < n ; i++){
-            for (int j = i ; j < n ; j++){
-                if (arr[i]>arr[j]){
-                    swap(arr[i], arr[j]);
+    
+        for (int i =0 ; i < n-1 ; i++){
+            int min=i;
+            for (int j = i+1 ; j < n ;j++){
+                if (arr[j]<arr[min]){
+                    min = j;
                 }
             }
+            swap(arr[min], arr[i]);
         }
-
         for (int i = 0 ; i < n ; i++){
             cout<<arr[i]<<" ";
         }
