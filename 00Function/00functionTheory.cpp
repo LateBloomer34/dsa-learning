8bf4e860a4d1f667a43c3a7fcb65a9841aea1186
LateBloomer34 <himnariyal30@gmail.com>
2025-06-04 11:05:09 +0530

    function basics

---
+++ 00Function/00functionTheory.cpp
@@ -0,0 +1,7 @@
+/*
+Function is a resuable piece of code that perform a specific task
+the main purpose of function is - resuable piece of code
+                                    easy to maintain 
+                                    easy to debug
+                                    easy to read or understand
+*/
