c0bbf5f5c833ba7bd369e79ad8775679a6d74ce6
LateBloomer34 <himnariyal30@gmail.com>
2025-06-04 14:02:16 +0530

    prime number checker

--- 00Function/06PrimeCheck.cpp
+++ 00Function/06PrimeCheck.cpp
@@ -3,22 +3,28 @@ using namespace std;
 
 int primeCheck(int num ){
     if (num<=1){
-        cout<<"number is not prime";
-        return 0;
+        // cout<<"number is not prime";
+        return false;
     }
         for(int i = 2 ; i*i <=num ; i++) {
             if (num%i==0){
-                cout<<"not prime";
-                return 0;
+                // cout<<"not prime";
+                return false;
             }
         }
-    cout<<"prime";
-    return 0;
+    // cout<<"prime";
+    return true;
 }
 
 int main (){
     int num ;
     cout<<"enter the number -: ";
     cin>>num;
-    primeCheck(num);
+    int result  = primeCheck(num);
+    if (result){
+        cout<<"prime";
+    }
+    else{
+        cout<<"not prime";
+    }
 }
